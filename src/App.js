db13be65ad27ce0523fe242c85390fddb132f96a
ap23hp <aprakhanna5@gmail.com>
2022-04-04 13:39:28 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+import CrudComponent from './CrudComponent';
+
+export default function App() {
+  return (
+    <div>
+      <CrudComponent />
+    </div>
+  );
+}
